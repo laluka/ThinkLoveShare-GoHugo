d0cb2a5813e32257b46375fa11b317063c4f856c
laluka <louka.jacques-chevallier@manomano.com>
2024-08-02 17:26:00 +0200

    polish

--- hugo_server.sh
+++ hugo_server.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ ! -f "hugo" ]; then
-    echo "Hugo found, cloning from docker! :)"
-    ./copy_hugo_elf.sh
-fi
-
 clear
-/bin/rm -rf resources
+/bin/rm -rf resources public
 ./hugo server --disableFastRender
